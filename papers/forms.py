595b748e6a4bfb69d407f5a24283a2931d862343
Sri Venkata Krishna Sourav V <krishnavitap@gmail.com>
2025-10-01 17:51:53 +0530

    Update forms.py

--- papers/forms.py
+++ papers/forms.py
@@ -4,14 +4,14 @@ from .models import QuestionPapers , ImportantTopics , Materials
 class QuestionPapersForm(forms.ModelForm):
     class Meta:
         model = QuestionPapers
-        fields = ['subject_name' , 'subject_code' , 'exam_type' , 'semester' , 'year' , 'file_choosen']
+        fields = ['question_paper_title' , 'subject_name' , 'subject_code' , 'exam_slot' , 'exam_type' , 'semester' , 'year' , 'file_choosen']
 
 class ImportantTopicsForm(forms.ModelForm):
     class Meta:
         model = ImportantTopics
-        fields = ['subject_name' , 'subject_code' , 'exam_type' , 'faculty_name' , 'file_choosen']
+        fields = ['important_topic_title' , 'subject_name' , 'subject_code' , 'exam_type' , 'faculty_name' , 'file_choosen']
 
 class MaterialsForm(forms.ModelForm):
     class Meta:
         model = Materials
-        fields = ['subject_name' , 'subject_code' , 'exam_type' , 'faculty_name' , 'file_choosen']
+        fields = ['study_material_title' , 'subject_name' , 'subject_code' , 'exam_type' , 'faculty_name' , 'file_choosen']
